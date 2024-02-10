a9e23ec785704685c3abb0f9a7324072c4233855
masaharu-nakano <nakanomyyh@hotmail.co.jp>
2024-02-10 10:19:50 +0900

    Initial commit

---
+++ build/carto_test/launch/serial_launch.py
@@ -0,0 +1,1 @@
+/home/masa/slam_ws/src/carto_test/launch/serial_launch.py
---
+++ src/carto_test/launch/serial_launch.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='serial_arduino',
+            namespace='serial_arduino',
+            executable='serial_test2',
+        ),
+        
+    ])
